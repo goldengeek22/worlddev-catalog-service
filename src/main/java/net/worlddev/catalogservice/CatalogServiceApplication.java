411c8ca36bf1f3ed2c9fbcc0f658ba14a9056997
Alexandre AMEVOR <amevoralexandre@gmail.com>
2023-06-20 14:15:41 +0000

    Add k8s resoucres for deployment

--- src/main/java/net/worlddev/catalogservice/CatalogServiceApplication.java
+++ src/main/java/net/worlddev/catalogservice/CatalogServiceApplication.java
@@ -3,7 +3,6 @@ package net.worlddev.catalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @author Alexandre AMEVOR
